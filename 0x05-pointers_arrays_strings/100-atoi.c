3ee1de85ddf01eb3b02378afae9f4e1db9e33a0f
Attomjohn <attomjohn@gmail.com>
2023-07-01 20:21:35 -0700

    11. Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,7 +7,7 @@
 */
 int _atoi(char *s)
 {
-int i, d, n, len, f, digit;
+ int i, d, n, len, f, digit;
 i = 0;
 d = 0;
 n = 0;
@@ -24,7 +24,7 @@ if (s[i] >= '0' && s[i] <= '9')
 {
 digit = s[i] - '0';
 if (d % 2)
-igit = -digit;
+digit = -digit;
 n = n * 10 + digit;
 f = 1;
 if (s[i + 1] < '0' || s[i + 1] > '9')
@@ -37,3 +37,4 @@ if (f == 0)
 return (0);
 return (n);
 }
+i
